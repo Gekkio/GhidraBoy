63dfccb3bdf0902e8695bf9f3194f493df920dad
Joonas Javanainen <joonas.javanainen@gmail.com>
2020-10-27 22:55:28 +0200

    Bump copyright year

--- src/main/java/fi/gekkio/ghidraboy/GameBoyUtils.java
+++ src/main/java/fi/gekkio/ghidraboy/GameBoyUtils.java
@@ -1,4 +1,4 @@
-// Copyright 2019 Joonas Javanainen <joonas.javanainen@gmail.com>
+// Copyright 2019-2020 Joonas Javanainen <joonas.javanainen@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
