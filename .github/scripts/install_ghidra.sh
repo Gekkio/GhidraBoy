9ed6ba9598a1a86b1e8c8ae94c821e2157de0c56
Joonas Javanainen <joonas.javanainen@gmail.com>
2021-06-25 23:40:20 +0300

    Update Ghidra release URL

--- .github/scripts/install_ghidra.sh
+++ .github/scripts/install_ghidra.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-GHIDRA_ZIP="${GHIDRA_VERSION}_${GHIDRA_BUILD_DATE}.zip"
+GHIDRA_ZIP="ghidra_${GHIDRA_VERSION}_${GHIDRA_BUILD_DATE}.zip"
 
 cd "${HOME}"
-curl -sSL "https://ghidra-sre.org/${GHIDRA_VERSION}_${GHIDRA_BUILD_DATE}.zip" -O
+curl -sSL "https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_${GHIDRA_VERSION}_build/${GHIDRA_ZIP}" -O
 
 echo "${GHIDRA_SHA256} ${GHIDRA_ZIP}" | sha256sum -c
 unzip "${GHIDRA_ZIP}"
