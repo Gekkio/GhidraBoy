494d240dd666e31c199a7e92f378173957fe98d8
Joonas Javanainen <joonas.javanainen@gmail.com>
2022-03-16 17:33:03 +0200

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ import java.util.Properties
 
 plugins {
     java
-    kotlin("jvm") version "1.6.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    kotlin("jvm") version "1.6.10"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 repositories {
